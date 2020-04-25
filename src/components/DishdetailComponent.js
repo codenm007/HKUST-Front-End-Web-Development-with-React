dd05869fa47aee8878a6d6013f9421a0c64e884e
Nilanjan <n.majumder14@gmail.com>
2020-04-25 18:38:49 +0530

    Added dishdetail.js

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -7,8 +7,10 @@ class DishDetail extends Component {
         const comments = this.props.dishes.map((dish) => {
             return (
                 <div key={dish.id}>
-                    <CardText>- {dish.comments[dish.id].author}</CardText>
-                    <CardText>"{dish.comments[dish.id].comment}"</CardText>
+                    <CardText>- {dish.comments[dish.id].author}
+                    </CardText>
+                    <CardText>"{dish.comments[dish.id].comment}"
+                    </CardText>
                 </div>
             );
         });
