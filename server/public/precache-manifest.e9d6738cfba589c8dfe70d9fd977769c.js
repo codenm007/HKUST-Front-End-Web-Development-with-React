cf47f0a4d872873337fe82d8b155cfe8a0d784ab
Nilanjan <n.majumder14@gmail.com>
2020-05-15 11:18:16 +0530

    Added webpack and build the hkust production build

---
+++ server/public/precache-manifest.e9d6738cfba589c8dfe70d9fd977769c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65d9e2290f5ec41615a1bc4327eb587b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3fc816c11ae5fb47c6ae",
+    "url": "/static/css/2.4173b2d3.chunk.css"
+  },
+  {
+    "revision": "e65827b6ee060e502e46",
+    "url": "/static/css/main.f78fc94c.chunk.css"
+  },
+  {
+    "revision": "3fc816c11ae5fb47c6ae",
+    "url": "/static/js/2.81b577d2.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.81b577d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e65827b6ee060e502e46",
+    "url": "/static/js/main.fc67723e.chunk.js"
+  },
+  {
+    "revision": "8ffa16b28231a96fa24b",
+    "url": "/static/js/runtime-main.4bbcf13b.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
